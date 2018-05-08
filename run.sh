8f479c7bd16ae9b54d7418ebc880cee0dd543b26
gitting-around <gitting-around@users.noreply.github.com>
2018-05-08 15:41:59 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,37 @@
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.25_10/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.5_10/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.0_10/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.75_10/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/1.0_10/launch_agents.launch 
+
+rosclean purge -y
+
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.25_25/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.5_25/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.0_25/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.75_25/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/1.0_25/launch_agents.launch 
+
+rosclean purge -y
+
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.25_50/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.5_50/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.0_50/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.75_50/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/1.0_50/launch_agents.launch 
+
+rosclean purge -y
+
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.25_75/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.5_75/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.0_75/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.75_75/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/1.0_75/launch_agents.launch 
+
+rosclean purge -y
+
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.25_100/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.5_100/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.0_100/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/0.75_100/launch_agents.launch 
+timeout 22m roslaunch ~/catkin_ws/src/GITagent/Launch/1.0_100/launch_agents.launch 
